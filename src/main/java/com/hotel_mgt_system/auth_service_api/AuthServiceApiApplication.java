43e40aa96f231c5ae83f5e9b3b8f307055677740
Suwantha Darshana <99712377+SuwanthaDarshana@users.noreply.github.com>
2025-10-10 09:24:26 +0530

    init

---
+++ src/main/java/com/hotel_mgt_system/auth_service_api/AuthServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.hotel_mgt_system.auth_service_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServiceApiApplication.class, args);
+	}
+
+}
