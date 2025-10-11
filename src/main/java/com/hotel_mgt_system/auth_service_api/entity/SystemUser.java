55b8032b08923c0fe5326882fc4ec24a3519de03
Suwantha Darshana <99712377+SuwanthaDarshana@users.noreply.github.com>
2025-10-12 02:28:06 +0530

    add system user entity

---
+++ src/main/java/com/hotel_mgt_system/auth_service_api/entity/SystemUser.java
@@ -0,0 +1,62 @@
+package com.hotel_mgt_system.auth_service_api.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "system_user")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
+public class SystemUser {
+    @Id
+    @Column(name = "id", length = 80, nullable = false)
+    private Long id;
+
+    @Column(name = "keycloak_id", length = 80, nullable = false)
+    private String keycloakId;
+
+    @Column(name = "first_name", length = 80, nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", length = 80, nullable = false)
+    private String lastName;
+
+    @Column(name = "email", length = 100, nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "contact", length = 20)
+    private String contact;
+
+    @Column(name = "is_active")
+    private boolean isActive;
+
+    @Column(name = "is_account_non_expired")
+    private boolean isAccountNonExpired;
+
+    @Column(name = "is_account_non_locked")
+    private boolean isAccountNonLocked;
+
+    @Column(name = "is_credentials_non_expired")
+    private boolean isCredentialsNonExpired;
+
+    @Column(name = "is_enabled")
+    private boolean isEnabled;
+
+    @Column(name = "is_email_verified")
+    private boolean isEmailVerified;
+
+    @Column(name = "created_at")
+    private Instant createdAt;
+
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+
+}
