ae2bd85fcfd3e870186461553515afdbf0f08e6a
Suwantha Darshana <99712377+SuwanthaDarshana@users.noreply.github.com>
2025-10-23 20:51:25 +0530

    add Email template handler

--- src/main/java/com/hotel_mgt_system/auth_service_api/util/EmailTemplateHelper.java
+++ src/main/java/com/hotel_mgt_system/auth_service_api/util/EmailTemplateHelper.java
@@ -1,4 +1,20 @@
 package com.hotel_mgt_system.auth_service_api.util;
 
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
 public class EmailTemplateHelper {
+
+    public String loadHtmlTemplate(String templateName){
+        try {
+            ClassPathResource resource = new ClassPathResource(templateName);
+            byte[] fileData = resource.getInputStream().readAllBytes();
+            return new String(fileData, StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return " ";
+        }
+    }
 }
